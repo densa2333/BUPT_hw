3801628910942112099ea81bd53482b5874de168
Densa <2041849656@qq.com>
2022-03-18 17:54:14 +0800

    20220318

---
+++ 计导课程设计/2022（13-15）计导课程设计 链表作业-2/1-链表归并.c
@@ -0,0 +1,142 @@
+/*题目描述
+已知有两个递增的正整数序列A和B，序列中元素个数未知，同一序列中不会有重复元素出现，有可能某个序列为空。
+现要求将序列B归并到序列A中，且归并后序列A的数据仍然按递增顺序排列。
+如果序列B中某些数据在序列A中也存在，则这些数据所在节点仍然留在序列B中，而不被归并到序列A中；
+否则这些数据所在节点将从序列B中删除，添加到序列A中。
+
+要求：
+建立两个单链表A、B用于存储两个正整数序列，然后按照题目的要求，将链表B中的元素归并到链表A中。
+在归并的过程中，不要释放B中的节点空间、然后建立新节点，而要改变指针的指向，使元素从B中删除并添加到A中。
+正整数序列按照递增顺序输入，用-1作为结束标志，注意-1不算这个正整数序列中的元素（不要统计-1）。
+在程序结束前要释放链表A、B中的所有节点。
+
+输入
+依次输入两个递增的正整数序列A和B，序列元素的个数未知，但以输入“-1”结束，每个正整数序列占一行。
+
+输出
+处理后的链表A中的元素，占一行；然后是处理后的链表B中的元素，占一行。
+每行的每个元素后有一个空格，注意最后一个元素后只有换行符，
+如果某个链表为空则，则输出“There is no item in X list.”
+数据最多的测试用例节点数在100这个数量级，所有整数可以用int型存储。
+请注意输入输出格式。
+
+样例输入 Copy
+Sample 1:
+1 3 4 5 6 7 -1
+2 3 6 8 9 10 11-1
+
+Sample 2:
+-1
+-1
+
+样例输出 Copy
+Sample 1:
+The new list A:1 2 3 4 5 6 7 8 9 10 11
+The new list B:3 6
+
+Sample 2:
+There is no item in A list.
+There is no item in B list.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define ElementType int
+#define Null -1
+
+typedef struct LNode * List;
+struct LNode {
+    ElementType Data;
+    List Next;
+};
+
+List ReadList();
+int Exist(List, ElementType);
+void MergeList(List, List);
+void PrintList(List, char);
+
+int main(void)
+{
+    List A = ReadList();
+    List B = ReadList();
+    MergeList(A, B);
+    PrintList(A, 'A');
+    PrintList(B, 'B');
+    return 0;
+}
+
+List ReadList()
+{
+    ElementType x;
+    List front, rear, cur;
+    front = malloc(sizeof(struct LNode));
+    front->Data = Null;
+    front->Next = NULL;
+    rear = front;
+    while (scanf("%d", &x)) {
+        if (x == Null) break;
+        cur = malloc(sizeof(struct LNode));
+        cur->Data = x;
+        cur->Next = NULL;
+        rear->Next = cur;
+        rear = cur;
+    }
+    return front;
+}
+
+int Exist(List L, ElementType x)
+{
+    for (List p = L; p; p = p->Next) {
+        if (p->Data == x) return 1;
+    }
+    return 0;
+}
+
+void MergeList(List A, List B)
+{
+    //对于链表往往需要当前节点指针和前继节点指针，有时还需要临时指针
+    List first1, first2, pre1, pre2, tmp;
+    first1 = A->Next;
+    first2 = B->Next;
+    pre1 = A;
+    pre2 = B;
+    // 归并部分画图理解 重点！
+    while (first1 && first2) {
+        if (first2->Data < first1->Data && !Exist(first1, first2->Data)) {
+            tmp = first2->Next;
+            first2->Next = first1;
+            pre1->Next = first2;
+            pre1 = first2;
+            pre2->Next = tmp;
+            first2 = tmp;
+        } else if (Exist(first1, first2->Data)) {
+            pre2 = first2;
+            first2 = first2->Next;
+        } else {
+            pre1 = first1;
+            first1 = first1->Next;
+        }
+    }
+    if (first2) {
+        pre1->Next = first2;
+        pre2->Next = NULL;
+    }
+}
+
+void PrintList(List L, char LName)
+{
+    int flag = 0;
+    if (L->Next == NULL)
+        printf("There is no item in %c list.", LName);
+    else {
+        printf("The new list %c:", LName);
+        for (List p = L->Next; p; p = p->Next) {
+            if (flag)
+                printf(" %d", p->Data);
+            else {
+                printf("%d", p->Data);
+                flag = 1;
+            }
+        }
+    }
+    printf("\n");
+}
---
+++ 计导课程设计/2022（13-15）计导课程设计 链表作业-2/2-拆分链表.c
@@ -0,0 +1,184 @@
+/*
+题目描述
+已知有一个乱序的字符序列L，序列中的字符可能是英文字母、数字字符或其它字符，字符的个数未知，每个字符之间用空格分开。
+字符序列用“-1”作为输入结束标志，这里你要把-1当做一个字符串对待，并且不算作字符序列中的元素。
+如下即为一个合法的字符序列：“a c 3 b a d 6 , & j m 8 7 2 V -1”。
+你的任务是将这个字符序列拆分为三个独立的序列A、B和C，其中序列A存放序列L中的字母，序列B存放序列L中的数字，序列C存放序列L中的其他字符，
+然后，将序列A、B和C分别按照ASCII码的大小关系进行升序排序。最终序列L将变为空序列。
+
+要求：
+建立四个单链表，分别存储序列L、A、B、C中的元素。字符序列的输入用“-1”作为结束标志。
+建立链表L时，建议使用scanf(“%s”,s);来读取字符序列中的字符，即把单独的字符看做一个字符串读取。
+当L建立后，你要按照问题描述中所述，将L拆分为A、B、C三个链表，然后对每个链表都进行排序，这部分的操作都应该是对指针进行修改，而不是删除节点与建立新节点。
+在程序结束前要释放链表A、B、C中的所有节点。
+
+输入
+一个乱序的字符序列，序列元素的个数未知，以输入“-1”结束，输入“-1”前可能没有其它元素，每个字符序列占一行。
+
+输出
+链表A中的元素，占一行；然后是链表B中的元素，占一行。最后是链表C中的元素，占一行。每
+行的每个元素后有一个空格，注意最后一个元素后只有换行符，如果某个链表为空则，则输出“There is no item in X list.”
+数据最多的测试用例节点数在100这个数量级。
+请注意输入输出格式。
+
+样例输入 Copy
+Sample 1:
+a c 3 b a d 6 , & j m 8 7 2 V -1
+
+Sample 2:
+z m v 1 a K 2 m p 9 a 0 a d -1
+
+样例输出 Copy
+Sample 1:
+The list A is: V a a b c d j m
+The list B is: 2 3 6 7 8
+The list C is: & ,
+
+Sample 2:
+The list A is: K a a a d m m p v z
+The list B is: 0 1 2 9
+There is no item in C list.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ElementType char
+
+typedef struct LNode * List;
+struct LNode {
+    ElementType Data;
+    List Next;
+};
+
+List ReadList();
+List CutList(List, int);
+void BubbleSortList(List);
+void PrintList(List, char);
+void FreeList(List);
+
+int main(void)
+{
+    List L, A, B, C;
+    L = ReadList();
+    A = CutList(L, 1);
+    B = CutList(L, 2);
+    C = CutList(L, 3);
+    PrintList(A, 'A');
+    PrintList(B, 'B');
+    PrintList(C, 'C');
+    FreeList(L);
+    FreeList(A);
+    FreeList(B);
+    FreeList(C);
+    return 0;
+}
+
+List ReadList()
+{
+    List front, rear, cur;
+    front = malloc(sizeof(struct LNode));
+    front->Data = '\0';
+    front->Next = NULL;
+    rear = front;
+    char s[3];
+    scanf("%s", s);
+    getchar();
+    while (strcmp("-1", s)) {
+        cur = malloc(sizeof(struct LNode));
+        cur->Data = s[0];
+        cur->Next = NULL;
+        rear->Next = cur;
+        rear = cur;
+        scanf("%s", s);
+        getchar();
+    }
+    return front;
+}
+
+//链表的冒泡排序模板
+void BubbleSortList(List L)
+{
+    if (L->Next) {
+        List tmp, pre, cur, next, end = NULL;
+        while (end != L->Next) {
+            for (pre = L, cur = pre->Next, next = cur->Next;
+                next != end; 
+                pre = pre->Next, cur = cur->Next, next = next->Next) {
+                if (cur->Data > next->Data) {
+                    cur->Next = next->Next;
+                    next->Next = cur;
+                    pre->Next = next;
+                    tmp = next;
+                    next = cur;
+                    cur = next;
+                }
+            }
+            end = cur;
+        }
+    }
+}
+
+List CutList(List L, int a)
+{
+    List first1, pre1 , front2, rear2, cur;
+    pre1 = L;
+    first1 = pre1->Next;
+    front2 = malloc(sizeof(struct LNode));
+    front2->Data = '\0';
+    front2->Next = NULL;
+    rear2 = front2;
+    //分割合并实现
+    while (first1) {
+        char x = first1->Data;
+        if ((x >= 'A' && x <= 'Z' || x >= 'a' && x <='z') && a == 1) {
+            rear2->Next = first1;
+            rear2 = first1;
+            first1 = first1->Next;
+            pre1->Next = first1;
+            rear2->Next = NULL;
+        } else if ((x >= '0' && x <= '9') && a == 2) {
+            rear2->Next = first1;
+            rear2 = first1;
+            first1 = first1->Next;
+            pre1->Next = first1;
+            rear2->Next = NULL;
+        } else if (a == 3) {
+            rear2->Next = first1;
+            rear2 = first1;
+            first1 = first1->Next;
+            pre1->Next = first1;
+            rear2->Next = NULL;
+        } else {
+            pre1 = first1;
+            first1 = first1->Next;
+        }
+    }
+    BubbleSortList(front2);
+    return front2;
+}
+
+void PrintList(List L, char X)
+{
+    if (L->Next == NULL)
+        printf("There is no item in %c list.", X);
+    else {
+        printf("The list %c is:", X);
+        List p = L->Next;
+        while (p) {
+            printf(" %c", p->Data);
+            p = p->Next;
+        }
+    }
+    printf("\n");
+}
+
+void FreeList(List L)
+{
+    List tmp = L;
+    while (L) {
+        tmp = L;
+        L = L->Next;
+        free(tmp);
+    }
+}
